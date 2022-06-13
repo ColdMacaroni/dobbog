910857690c742b21ac0339b5249e47a365a5974a
muffin <diegoxe7117@gmail.com>
2022-06-13 14:51:35 +1200

    motor configs

---
+++ main.c
@@ -0,0 +1,4 @@
+#pragma config(Motor,  port1,           frontLeftMotor,    tmotorNormal, openLoop)
+#pragma config(Motor,  port2,           frontRightMotor,    tmotorNormal, openLoop, reversed)
+#pragma config(Motor,  port3,           backLeftMotor,     tmotorNormal, openLoop)
+#pragma config(Motor,  port4,           backRightMotor,     tmotorNormal, openLoop, reversed)
